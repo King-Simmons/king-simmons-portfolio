9c3c65e441827bc28384767c046978a0cc032b57
King Simmons <kd3business@gmail.com>
2026-01-24 18:37:52 -0600

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,17 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-  transform: {
-    "^.+\\.(ts|tsx)$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.json" }]
-  },
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1"
-  }
+    // Handle module aliases
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 };
+
+module.exports = createJestConfig(customJestConfig);
